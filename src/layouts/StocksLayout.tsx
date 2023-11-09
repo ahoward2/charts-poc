40a437ed61a3a0120c24452f42e685e88b4f3460
Austin Howard <ahoward@austinhoward.tech>
2023-11-08 21:46:14 -0500

    fixed headers

--- src/layouts/StocksLayout.tsx
+++ src/layouts/StocksLayout.tsx
@@ -13,16 +13,16 @@ export const StocksLayout = ({ children }: Props) => {
   const { id } = router.query;
 
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-full mt-24">
       {router.pathname !== "/security" && (
         <>
-          <div className="w-full h-12 bg-white flex border-b items-center px-6 sticky top-0">
+          <div className="w-screen h-12 z-20 bg-white flex border-b items-center px-6 fixed top-12">
             <div className="text-black h-6 pr-6">
               {getSecurityById(Number(id))?.name}
             </div>
           </div>
           {id && (
-            <nav className="h-12 flex items-center px-6 border-b sticky">
+            <nav className="h-12 flex items-center z-20 px-6 border-b fixed top-24 w-screen bg-white">
               <NavItem href={`/security/${id}`}>Summary</NavItem>
               <NavItem href={`/security/${id}/report-builder`}>
                 Report Builder
